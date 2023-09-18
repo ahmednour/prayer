0c65552e8a0d7b5c8e3af2d28ac1262e012be160
Ahmed Nour <darklevel152@gmail.com>
2023-09-18 14:42:20 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Container } from '@mui/material';
+import './App.css'
+import MainContent from './components/MainContent';
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Container>
+          <MainContent/>
+        </Container>
+      </div>      
+    </>
+  )
+}
+
+export default App
